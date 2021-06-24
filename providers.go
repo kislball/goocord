cae35df2f9ba2506d8fe28707d8b4c7f608321c7
Кислый Кислюк <kislball@yahoo.com>
2021-06-24 23:21:16 +0300

    Add REST interfaces

--- providers.go
+++ providers.go
@@ -1,6 +1,8 @@
 package goocord
 
-import "errors"
+import (
+	"errors"
+)
 
 // Providers represent a set of providers used by Client
 type Providers struct {
@@ -20,4 +22,19 @@ type CacheProvider interface {
 	Clear(namespace string) error                              // Clear clears entire namespace
 	GetAll(namespace string) (map[string]interface{}, error)   // GetAll gets all key/value pairs
 	Total(namespace string) (int, error)                       // Total returns total amount of k/v pairs
+}
+
+// RestProvider represents a requester which sends requests to Discord.
+// Internally, GooCord doesn't implement ratelimiting, RestProviders are responsible for that
+type RestProvider interface {
+	UseToken(token string)                                                                                     // UseToken sets a token to use
+	UseAPI(url string)                                                                                         // UseAPI sets an API url
+	Request(method string, endpoint string, headers map[string]string, body interface{}) (RestResponse, error) // Request sends a request to Discord API
+}
+
+// RestResponse represents a response from Discord API
+type RestResponse struct {
+	StatusCode int               // HTTP status code
+	Headers    map[string]string // HTTP Headers
+	Body       interface{}       // Body
 }
