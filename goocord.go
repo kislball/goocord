d28db938d23ac5ccc346a26d2e621dc9369b2cb8
Кислый Кислюк <kislball@yahoo.com>
2021-06-27 20:56:38 +0300

    Gram error fix

--- goocord.go
+++ goocord.go
@@ -1,5 +1,5 @@
 /*
-Package goocord contains GooCord is a simple, robust and versatile Discord-API
+Package goocord contains GooCord - a simple, robust and versatile Discord-API
 library made for Go.
 */
 package goocord
