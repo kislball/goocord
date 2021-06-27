63253097cc208375e5824aa8b7d792fadfe02828
Кислый Кислюк <kislball@yahoo.com>
2021-06-27 18:32:23 +0300

    Add documentation

--- types.go
+++ types.go
@@ -2,6 +2,6 @@ package goocord
 
 // Base represents something that can be got from a primitive
 type Base interface {
-	FromPrimitive(struct{})
-	ToPrimitive() struct{}
+	FromPrimitive(struct{}) // FromPrimitive forces structure to base on primitive
+	ToPrimitive() struct{} // ToPrimitive converts structure to primitive
 }
