a7b4db84cfd9a58e7051a9feb78fb4416ab02e2d
Кислый Кислюк <kislball@yahoo.com>
2021-06-29 16:43:24 +0300

    Flag test fixes

--- cache.go
+++ cache.go
@@ -1,7 +1,6 @@
 package goocord
 
 import (
-	"errors"
 	"sync"
 )
 
--- flags_test.go
+++ flags_test.go
@@ -8,15 +8,19 @@ import (
 var f Flags = Flags{0}
 
 func TestFlags_Add(t *testing.T) {
-	f.Add(1 >> 7)
-	f.Add(1 >> 8)
-	if f.Flags != 1>>7|1>>8 {
+	f.Add(1 << 7)
+	f.Add(1 << 8)
+	if f.Flags != 1<<7|1<<8 {
 		t.Error(fmt.Sprintf("expected - %d, got - %d", 1>>7|1>>8, f.Flags))
 	}
 }
 
 func TestFlags_Has(t *testing.T) {
-	if !f.Has(1>>7) || !f.Has(1>>8) {
+	if !f.Has(1<<7) || !f.Has(1<<8) {
 		t.Error("shit")
+	}
+
+	if f.Has(1<<9) {
+		t.Error("expected bitfield not to include 1<<9")
 	}
 }
